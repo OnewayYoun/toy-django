f5d0f195131a52d18ae8818fd6258bc2b87af508
OnewayYoun <onewayy91@gmail.com>
2021-07-17 22:22:08 +0900

    sending data to another page(action)

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,10 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     # name = 'Yoon'
-    context = {
-        'last_name': 'Yoon',
-        'age': 31,
-        'nationality': 'Korea'
-    }
+    # context = {
+    #     'last_name': 'Yoon',
+    #     'age': 31,
+    #     'nationality': 'Korea'
+    # }
     # return render(request, 'index.html', {'last_name': name})
-    return render(request, 'index.html', context)
+    # return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+
+def counter(request):
+    text = request.GET['text']
+    text_amount = len(text.split())
+
+    return render(request, 'counter.html', {'amount': text_amount})
