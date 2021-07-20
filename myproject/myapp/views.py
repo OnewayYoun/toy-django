5627709e42559b888ccd2bad1c9685e4404852c2
OnewayYoun <onewayy91@gmail.com>
2021-07-20 16:58:46 +0900

    Django Admin Panel & Manipulation of Database

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -15,32 +15,33 @@ def index(request):
     # return render(request, 'index.html', context)
 
     # Models
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Yoon'
-    feature1.details = 'Hi! I"m Yoon'
-    feature1.is_true = True
+    # feature1 = Feature()
+    # feature1.id = 0
+    # feature1.name = 'Yoon'
+    # feature1.details = 'Hi! I"m Yoon'
+    # feature1.is_true = True
+    #
+    # feature2 = Feature()
+    # feature2.id = 1
+    # feature2.name = 'Kim'
+    # feature2.details = 'Hi! I"m Kim'
+    # feature2.is_true = True
+    #
+    # feature3 = Feature()
+    # feature3.id = 2
+    # feature3.name = 'Lee'
+    # feature3.details = 'Hi! I"m Lee'
+    # feature3.is_true = False
+    #
+    # feature4 = Feature()
+    # feature4.id = 3
+    # feature4.name = 'Park'
+    # feature4.details = 'Hi! I"m Park'
+    # feature4.is_true = True
+    #
+    # features = [feature1, feature2, feature3, feature4]
 
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Kim'
-    feature2.details = 'Hi! I"m Kim'
-    feature2.is_true = True
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Lee'
-    feature3.details = 'Hi! I"m Lee'
-    feature3.is_true = False
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Park'
-    feature4.details = 'Hi! I"m Park'
-    feature4.is_true = True
-
-    features = [feature1, feature2, feature3, feature4]
-
+    features = Feature.objects.all()    #getting all data(objects) from DB
     return render(request, 'index.html', {'features': features})
 
 
