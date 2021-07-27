fabe7006b86d581f6d3496ebc94642d695bbda42
OnewayYoun <onewayy91@gmail.com>
2021-07-27 16:52:39 +0900

    User Registration

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Feature
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -51,3 +53,27 @@ def counter(request):
     text_amount = len(text.split())
 
     return render(request, 'counter.html', {'amount': text_amount})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save();
+                return redirect('index')
+        else:
+            messages.info(request, 'Password Not The Same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
