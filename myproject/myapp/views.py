75a09c598383ce14906c52221be7a020a291b8f7
OnewayYoun <onewayy91@gmail.com>
2021-07-27 21:31:37 +0900

    User log in/out

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -71,9 +71,30 @@ def register(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save();
-                return redirect('index')
+                return redirect('login')
         else:
             messages.info(request, 'Password Not The Same')
             return redirect('register')
     else:
         return render(request, 'register.html')
+
+# 로그인 views
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+# 로그아웃 views
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
