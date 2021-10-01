c9714ec050ac6b63bac84625551885e58c174f29
OnewayYoun <onewayy91@gmail.com>
2021-10-01 18:18:44 +0900

    Connecting postgresql

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -97,10 +97,20 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': 'qhdrlfdyadya2',
+        'HOST': 'localhost'
     }
 }
 
